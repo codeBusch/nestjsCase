8140ae934161db94175cccb03dca92a19b1b8b54
Bahri.Busch <turkbahri19@hotmail.com>
2024-06-15 19:11:11 +0300

    user controller deleted

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController],
   imports:[TypeOrmModule.forFeature([User])],
   exports:[UsersService],
 
